25f40df4b85221e1fb29d87d4da6b54383555f98
Fady Mikhael <fadymax24@gmail.com>
2023-12-05 16:51:50 +0100

    Install EslInt

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,19 @@ function Todolist() {
     if (task.trim() !== '') {
       setTaskList([...taskList, task]);
       setTask(''); 
-      console.log(AddTaskfnc)
     }
   }
   return (
-     
-    <div className="Addtask">
+<div className="Addtask">
     <h1 className='text-3xl text-red-500 text-center'>Todo list</h1>
     <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
      <button className='text' onClick={AddTaskfnc}> Add task  </button>
      <ul>
         {taskList.map((taskItem, index) => (
            <li key={index}>{taskItem}</li>
-        
         ))} 
       </ul>
     </div>
-    
   );
 }
-
 export default Todolist;
