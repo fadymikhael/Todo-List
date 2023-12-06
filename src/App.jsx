f57cbfdd3826079af2c9f6c0e3c12f52b152d972
FADY MIKHAEL <fadymax24@gmail.com>
2023-12-06 11:24:55 +0100

    Add fuction Delete task

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,31 @@ function Todolist() {
   const [task, setTask] = useState('');
   const [taskList, setTaskList] = useState([]);
 
-   function AddTaskfnc () {
+   function AddTaskfnc() {
     if (task.trim() !== '') {
       setTaskList([...taskList, task]);
-      setTask('');
+    setTask('');
     }
   }
-
+  function DeleteTask(index){
+      const updatedTasks = [...taskList];
+      updatedTasks.splice(index,1);
+      setTaskList(updatedTasks);
+    }
   return (
       <div className="Addtask">
-          <h1 className='text-3xl text-red-500 text-center'>
-              Todo list
-          </h1>
-          <input
-              onChange={(e) => setTask(e.target.value)}
-              type="text"
-              value={task}
-          />
+          <h1 className='text-3xl text-red-500 text-center'>Todo list</h1>
+
+          <input onChange={(e) => setTask(e.target.value)} type="text"value={task}/>
           <button className='text'onClick={AddTaskfnc}>Add task</button>
-          <ul>
-              {taskList.map((taskItem, index) => (
-                  <li key={index}>
-                      {taskItem}
-                  </li>
+
+          <div> {taskList.map((task, index) => (
+          <div key={index}>{task}
+            <button onClick={() => DeleteTask(index)}>Delete Task</button>
+          </div>
         ))}
-          </ul>
       </div>
+    </div>
   );
 }
 export default Todolist;
