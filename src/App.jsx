e7c95ae9e41a229e3475449f7784a66bebb23c4f
FADY MIKHAEL <fadymax24@gmail.com>
2023-12-06 13:15:26 +0100

    Add fuction Edit Task

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,55 @@ import './index.css';
 function Todolist() {
   const [task, setTask] = useState('');
   const [taskList, setTaskList] = useState([]);
+  const [editIndex, setEditIndex] = useState(); 
+  const [editdTask, setEditdTask] = useState(''); 
 
-   function AddTaskfnc() {
+  function AddTaskfnc() {
     if (task.trim() !== '') {
       setTaskList([...taskList, task]);
-    setTask('');
+      setTask('');
     }
   }
-  function DeleteTask(index){
-      const updatedTasks = [...taskList];
-      updatedTasks.splice(index,1);
-      setTaskList(updatedTasks);
-    }
+
+  function DeleteTask(index) {
+    const updatedTasks = [...taskList];
+    updatedTasks.splice(index, 1);
+    setTaskList(updatedTasks);
+  }
+
+  function EditTask(index) {
+    setEditIndex(index); 
+    setEditdTask(taskList[index]);
+  }
+
+  function SaveTask() {
+    const updatedTasks = [...taskList];
+    updatedTasks[editIndex] = editdTask; 
+    setTaskList(updatedTasks);
+    setEditdTask(''); 
+    setEditIndex(); 
+  }
+
   return (
-      <div className="Addtask">
-          <h1 className='text-3xl text-red-500 text-center'>Todo list</h1>
-
-          <input onChange={(e) => setTask(e.target.value)} type="text"value={task}/>
-          <button className='text'onClick={AddTaskfnc}>Add task</button>
-
-          <div> {taskList.map((task, index) => (
-          <div key={index}>{task}
-            <button onClick={() => DeleteTask(index)}>Delete Task</button>
+    <div className="Addtask">
+      <h1 className='text-3xl text-red-500 text-center'>Todo list</h1>
+      <input onChange={(e) => setTask(e.target.value)} type="text" value={task} />
+      <button className='text' onClick={AddTaskfnc}>Add task</button>
+      <div>
+        {taskList.map((taskItem, index) => (
+          <div key={index}>
+            {editIndex === index ? (
+              <>
+                <input onChange={(e) => setEditdTask(e.target.value)} type="text" value={editdTask} />
+                <button onClick={SaveTask}>Save</button>
+              </>
+            ) : (
+              <>
+                {taskItem}
+                <button onClick={() => DeleteTask(index)}>Delete Task</button>
+                <button onClick={() => EditTask(index)}>Edit Task</button>
+              </>
+            )}
           </div>
         ))}
       </div>
