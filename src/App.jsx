ce1d2a17e5ff70691d44aefa839ce4fa18909cd8
FADY MIKHAEL <fadymax24@gmail.com>
2023-12-07 14:58:21 +0100

    c'est fait un compososant de bouton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react'
 import './index.css'
 import './App.css'
-
-// coucou
+import PropTypes from 'prop-types'
 
 function Todolist() {
   const [task, setTask] = useState('')
   const [taskList, setTaskList] = useState([])
   const [editIndex, setEditIndex] = useState()
   const [editdTask, setEditdTask] = useState('')
+
+  const Button = ({ texte, onClick, type, index }) => {
+    return (
+      <button
+        className={`px-2 py-2 mr-2 rounded font-semibold text-white ${type}`}
+        onClick={() => onClick(index)}
+      >
+        {texte}
+      </button>
+    )
+  }
+  Button.propTypes = {
+    texte: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    type: PropTypes.string.isRequired,
+    index: PropTypes.number,
+  }
 
   function AddTaskfnc() {
     if (task.trim() !== '') {
@@ -44,56 +60,41 @@ function Todolist() {
   }
 
   return (
-    <div className="flex justify-center items-start h-sc">
+    <div className="flex justify-center items-start h-screen">
       <div className="w-80 h-70 flex flex-col items-center bg-red-400 mt-4 rounded-md">
         <h1 className="text-center text-3xl mb-4 font-serif font-semibold">Todo list</h1>
-        <div className="mb-4 flex items-center">
+        <div className="mb-3 flex items-center">
           <input
-            className="border border-black px-3 py-2 rounded "
+            className="border border-black px-3 py-2 mr-2  rounded "
             onChange={(e) => setTask(e.target.value)}
             type="text"
             value={task}
           />
-          <button
-            className="bg-green-500 ml-3 px-3 py-2 rounded text-white font-semibold"
-            onClick={AddTaskfnc}
-          >
-            Add task
-          </button>
+          <Button texte="Add task" onClick={AddTaskfnc} type="bg-green-500" />
         </div>
         <div>
           {taskList.map((taskItem, index) => (
-            <div key={index} className="bg-gray-200 p-3 mb-4 rounded-md">
+            <div key={index} className="bg-gray-200 p-3 mb-4 mr-1 ml-1 rounded-md">
               {editIndex === index ? (
                 <>
                   <input
-                    className="border border-black px-3 py-2 rounded"
+                    className="border border-black px-3 py-2 mr-2 rounded"
                     onChange={(e) => setEditdTask(e.target.value)}
                     type="text"
                     value={editdTask}
                   />
-                  <button
-                    className="bg-blue-500 ml-2 px-3 py-1 rounded text-white font-semibold"
-                    onClick={SaveTask}
-                  >
-                    Save
-                  </button>
+                  <Button texte="Save" onClick={SaveTask} type="bg-blue-500" index={index} />
                 </>
               ) : (
                 <>
                   {taskItem}
-                  <button
-                    className="bg-red-500 ml-2 px-3 py-1 rounded text-white font-semibold"
-                    onClick={() => confirmDeleteTask(index)}
-                  >
-                    Delete task
-                  </button>
-                  <button
-                    className="bg-blue-500 ml-2 px-3 py-1 rounded text-white font-semibold"
-                    onClick={() => EditTask(index)}
-                  >
-                    Edit task
-                  </button>
+                  <Button
+                    texte="Delete task"
+                    onClick={confirmDeleteTask}
+                    type="bg-red-500"
+                    index={index}
+                  />
+                  <Button texte="Edit task" onClick={EditTask} type="bg-blue-500" index={index} />
                 </>
               )}
             </div>
@@ -103,4 +104,5 @@ function Todolist() {
     </div>
   )
 }
+
 export default Todolist
